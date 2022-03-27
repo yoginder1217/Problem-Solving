78536d20e530b3046523bea79053f8c4c99fbdb5
Yoginder Singh Kushwaha <yogi.121@outlook.com>
2022-03-27 19:52:39 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+  {
+      int a;
+      cout<<"Size of integer : "<<sizeof(a)<<endl;
+
+      float b;
+      cout<<"Size of float : "<<sizeof(b)<<endl;
+
+      bool c;
+      cout<<"Size of bool : "<<sizeof(c)<<endl;
+
+      long int li;
+      cout<<"SIze of long int: "<<sizeof(li)<<endl;
+
+      short int si;
+      cout<<"size of short int: "<<sizeof(si)<<endl;
+
+      double d;
+      cout<<"size of double : "<<sizeof(d)<<endl;
+
+      long double ld;
+      cout<<"size of long double: "<<sizeof(ld)<<endl;
+
+      char w;
+      cout<<"size of character: "<<sizeof(w)<<endl;
+
+      string s;
+      cout<<"size of string: "<<sizeof(s)<<endl;
+
+      return 0;
+  }
